75cc0a2494959d06b6b087fcab9e952b30c4704e
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-13 11:17:42 +0330

    ✨ feat:Impletemend Card section.

---
+++ src/components/card/Card.style.ts
@@ -0,0 +1,21 @@
+export const cardStyle = function () {
+  return {
+    ".muiCard": {
+      maxWidth: "672px",
+      border: "1px solid #CECECE",
+      margin: "0 auto",
+
+      ".gridTitle": {
+        margin: "32px 0 16px 0",
+        paddingRight: "31px",
+
+        ".typographyTitle": {
+          marginBottom: "60px",
+          textAlign: "right",
+          fontSize: "18px",
+          fontWeight: "bold",
+        },
+      },
+    },
+  };
+};
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,31 @@
+import {
+  Grid,
+  Card as MuiCard,
+  CardHeader,
+  Typography,
+  CardContent,
+} from "@mui/material";
 import React from "react";
-const Card = function () {
-  return <></>;
+import { CardProps } from "./CardProps";
+const Card = function ({ title, children }: CardProps) {
+  return (
+    <Grid item xs>
+      <MuiCard variant="outlined">
+        <Grid item xs className="gridStyleCardHeader">
+          <CardHeader
+            className="styleCardHeader"
+            avatar={<img src="" alt="logo" />}
+          />
+        </Grid>
+        <Grid item xs className="gridStyleTitle">
+          <Typography>{title}</Typography>
+        </Grid>
+        <Grid item xs>
+          <CardContent>{children}</CardContent>
+        </Grid>
+      </MuiCard>
+    </Grid>
+  );
 };
 
 export default Card;
