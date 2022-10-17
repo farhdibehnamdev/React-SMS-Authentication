240b5a93e59528fb170d33454905a5bb7a60bc7f
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-10-17 18:18:05 +0330

    ♻️ refactor:Removed unnecessary codes.

--- src/components/signin/SignIn.tsx
+++ src/components/signin/SignIn.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import Card from "components/card/Card";
 import Form from "components/form/Form";
-import { Box } from "@mui/material";
 import { Grid } from "@mui/material";
-import boxStyle from "./SignIn.style";
+import boxStyle from "components/common/componentStyles";
 
 const SignIn = function () {
   return (
-    <Box sx={boxStyle}>
-      <Grid container>
-        <Card title="ورود | ثبت نام">
-          <Form
-            inputLabel="شماره موبایل خود را وارد کنید"
-            otpText={null}
-            forgetPasswordText={null}
-            isLoginForm={true}
-            refactorPhoneNumber={null}
-          />
-        </Card>
-      </Grid>
-    </Box>
+    <Grid item sx={boxStyle}>
+      <Card cardSize="672px" title="ورود | ثبت نام">
+        <Form
+          inputLabel="شماره موبایل خود را وارد کنید"
+          otpText={null}
+          forgetPasswordText={null}
+          isLoginForm={true}
+          refactorPhoneNumber={null}
+        />
+      </Card>
+    </Grid>
   );
 };
 
